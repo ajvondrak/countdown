a4f768de6e3e5127644eb0410522d33616b48aa9
Alex Vondrak <alex@alex-pc>
2013-10-11 16:31:31 -0700

    initial commit

---
+++ countdown.py
@@ -0,0 +1,16 @@
+"""
+Python does Countdown.
+
+Quoth https://en.wikipedia.org/wiki/Countdown_%28game_show%29
+
+    Countdown is a British game show involving word and number puzzles.
+
+    The two contestants in each episode compete in three disciplines: eleven
+    letters rounds, in which the contestants attempt to make the longest word
+    possible from nine randomly chosen letters; three numbers rounds, in which
+    the contestants must use arithmetic to reach a random target number from
+    six other numbers; and the conundrum, a buzzer round in which the
+    contestants compete to solve a nine-letter anagram.
+
+I was watching the show and decided that it'd be easy for a computer to win it.
+"""
