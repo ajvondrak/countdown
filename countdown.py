8ddea815d2885432246cab162851db165892e82c
Alex Vondrak <alex@alex-pc>
2013-10-11 16:42:26 -0700

    solve letters round

--- countdown.py
+++ countdown.py
@@ -14,3 +14,31 @@ Quoth https://en.wikipedia.org/wiki/Countdown_%28game_show%29
 
 I was watching the show and decided that it'd be easy for a computer to win it.
 """
+
+__all__ = ['letters_round', 'numbers_round', 'conundrum']
+
+words = [w.strip() for w in open('/usr/share/dict/words').readlines()]
+words = [w for w in words if len(w) <= len('countdown')]
+
+def letters_round(letters):
+    winning_length = len(winning_word(letters))
+    for word in possible_words(letters):
+        if len(word) == winning_length:
+            print(word)
+
+def winning_word(letters):
+    return max(possible_words(letters), key=len)
+
+def possible_words(letters):
+    for word in words:
+        if could_form(word, letters):
+            yield word
+
+def could_form(word, letters):
+    return same_letters(word, letters) and enough_of_each_letter(word, letters)
+
+def same_letters(word, letters):
+    return set(word).issubset(set(letters))
+
+def enough_of_each_letter(word, letters):
+    return all(word.count(l) <= letters.count(l) for l in letters)
